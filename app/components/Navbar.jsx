b85abcec05b4435be94e57bc5482da110062169e
mjfy93 <maria.jose.ferro@hotmail.com>
2025-09-11 15:37:36 -0500

    first commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,9 +6,13 @@ export default function Navbar() {
       <ul>
         <li>
           <Link to='/'>Home</Link>
-        </li>        <li>
+        </li>       
+        <li>
           <Link to='/about'>About</Link>
         </li>
+        <li>
+          <Link to='/contact'>Contact</Link>
+        </li> 
         <li>
           <Link to='/contact'>Contact</Link>
         </li>
