6b6eaefb90640e280fdd577d0fbd7921c8c92a65
WaficMikati <wrmikati@proton.me>
2025-09-10 20:58:12 -0500

    first commit

---
+++ app/root.jsx
@@ -0,0 +1,29 @@
+import './styles/root.css'
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Navbar />
+
+        <main className="container mt-4">
+          <Outlet />
+        </main>
+
+        <Footer />
+
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes.js
@@ -0,0 +1,5 @@
+export default [
+    {path: '/', file: 'routes/_index.jsx'},
+    {path: '/about', file: 'routes/about.jsx'},
+    {path: '/contact', file: 'routes/contact.jsx'}
+]
